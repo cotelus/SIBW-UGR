5a8641fea92642dba0a03c64c12a8261cfa7ae51
Luis Ángel Cotelo Leyva <cotelus@gmail.com>
2019-03-17 13:01:44 +0100

    Javascript completo

---
+++ p2/js/script.js
@@ -0,0 +1,121 @@
+var comments_displayed = false;
+forbiddenWord = "";
+
+var word_set =[
+    "sibw",
+    "profesor",
+    "asignatura",
+    "sistemas",
+    "casa",
+    "coche",
+    "perro",
+    "gato",
+    "mono",
+    "pepino"
+];
+
+// Muestra o no el div que tiene los comentarios y el formulario ( y desplaza el boton de comentarios)
+function toggleComments() {
+    var comments_button = document.getElementById('comment-button');
+    var comments = document.getElementById('comment-bar');
+    if(comments_displayed){
+        comments.classList.remove('comment-bar-active');
+        comments_displayed = false;
+        comments_button.classList.remove('active-comment-button');
+    } else {
+        comments.classList.add('comment-bar-active');
+        comments_displayed = true;
+        comments_button.classList.add('active-comment-button');
+    }
+}
+
+
+// Añade el comentario que se haya dejado en el formulario
+function addComment(event){
+    event.preventDefault();
+    var name = document.getElementById('name');
+    var message = document.getElementById('message');
+    var email = document.getElementById('email');
+    if(itsEmptyField(name) || itsEmptyField(email) || itsEmptyField(message)) {
+        alert("Hay un error con algún campo");
+        return false;
+    }
+
+    if(!itsCorrectEmail(email)){
+        alert("El e-mail no es válido");
+        return false;
+    }
+
+    var comments = document.getElementsByClassName('comment-section');
+    var date = (new Date()).toLocaleString('es-ES',{timeZone:'Europe/Madrid'});
+
+    comments[0].insertAdjacentHTML('beforeend', "\n" +
+    "<div class=\"comment\">\n" +
+    "   <h5>\n" +
+    "       <a href=\"mailto:"+email.value+"\">\n"+
+    "               "+name.value+"\n" +
+    "       </a>\n" +
+    "   </h5>\n" +
+    "   <span class=\"comment-subheading\">Escrito: <strong>"+date+"</strong></span>\n" +
+    "   <p class=\"comment-text\">\n" +
+    "       "+message.value+"\n" +
+    "   </p>\n" +
+    "</div>");
+
+    return false;
+}
+
+// Comprueba si un campo está vacio
+function itsEmptyField(field){
+    var value = field.value;
+    var vacio = false;
+
+    if(value.trim() == "" || value === null || value === undefined){
+        vacio = true;
+    }
+    
+    return vacio;
+}
+
+// Devuelve true si el email tiene una forma válida y false en caso contrario
+function itsCorrectEmail(email){
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email.value).toLowerCase());
+}
+
+// Almacena cada tecla pulsada:
+//              1.- Si la tecla pulsada no es el espacio, almacena la tecla
+//              2.- Si la tecla pulsada es el espacio, comprueba el string que haya almacenado
+function addStringtoForbidden(event){
+    if(event.keyCode != 32){
+        forbiddenWord += String.fromCharCode(event.keyCode).toLowerCase();
+    }
+    else{
+        checkForbiddenWord(forbiddenWord);
+        forbiddenWord = "";
+    }
+}
+
+// Comprueba si la palabra pertenece a las palabras prohibidas
+function checkForbiddenWord(word){
+    var i;
+    for(i = 0; i < word_set.length; i++){
+        if(word == word_set[i]  ){
+            censorWord(word);
+        }
+    }
+}
+
+// Cambia la palabra por una cadena de * de la misma longitud que la palabra prohibida
+function censorWord(word){
+    var text = document.getElementById("message");
+
+    var newWord = "";
+    for (var i = 0; i < word.length; i++){
+        newWord += "*";
+    }
+
+    var res = text.value.replace(word, newWord);
+
+    text.value = res;
+}
