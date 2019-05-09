694e1796719c722683eb3ef19c37f8a796b9229b
cotelus <cotelus@gmail.com>
2019-05-10 00:16:56 +0200

    event.php listo -> encargado de mostrar los datos del evento (Falta poner bien algunas cosas, pero la carga es buena)

---
+++ p3/server/html/event.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+    <!-- Se incluye el archivo de cabecera -->
+    <?php
+        $_GET['pageTitle']="Evento";
+        include("php/head.php");
+    ?>
+
+    <body>
+        <!-- Se incluye la cabecera de la página -->
+        <?php
+            include("php/header.php");
+        ?>
+
+        <!-- Aquí se van metiendo los componentes -->
+        <div id="main">
+            <?php
+                include("php/aside.php");
+            ?>
+        <!-- La sección "principal" en la que van los eventos, si se genera en la portada -->
+            <section id="event-list" class="main-section">
+                
+                <?php
+                    include("php/eventFull.php");
+                ?>
+            </section>
+        </div>
+
+        <?php
+            include("php/footer.php");
+        ?>
+    </body>
+</html>
